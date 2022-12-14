456f7bd015f349443d68f70290a9eaa60793bf1a
Shawn <sgiovanni1@yahoo.com>
2022-12-14 00:58:35 -0500

    fixed majjor issues

--- app/blueprints/auth/routes.py
+++ app/blueprints/auth/routes.py
@@ -18,12 +18,15 @@ def login():
     email = request.form['email']
     password = request.form['password']
 
+# def check_my_password(self, password):
+#         return check_password_hash(self.password, password)
+
     user=User.query.filter_by(email=email).first()
 
     #if user doesnt exist
     if user is None:
         return f'User with email {email} does not exist.'
-    elif user.password == password:
+    elif user.check_my_password(password):
         login_user(user)
         return redirect(url_for('main.home'))
     else:
@@ -41,7 +44,7 @@ def register():
     email =request.form['email']
     password =request.form['password']
     confirm_password =request.form['confirmPassword']
-    username =request.form['username']
+    user_name =request.form['username']
     first_name =request.form['firstName']
     last_name =request.form['lastName']
 
@@ -57,39 +60,39 @@ def register():
     else:
        
         try:
-            new_user = User(email=email, username=username, password=password, first_name=first_name, last_name=last_name)
+            new_user = User(email=email, user_name=user_name, password=password, first_name=first_name, last_name=last_name)
             new_user.hash_my_password(password)
             db.session.add(new_user)
             db.session.commit()
             print('User created successfully.', 'success')
             return redirect(url_for('auth.login'))
         except:
-            print('There was an error.', 'danger')
+            return 'there ws an error'
 
-    return render_template('register.html')
+        # return render_template('register.html')
 
-@app.route('/logout')
-def logout():
-    logout_user()
-    Print('Logged out successfully', 'success')
-    return redirect(url_for('auth.login'))   
+# @app.route('/logout')
+# def logout():
+#     logout_user()
+#     Print('Logged out successfully', 'success')
+#     return redirect(url_for('auth.login'))   
 
 
-@app.route('/reset-passowrd', methods=[ "GET","POST" ])
-def reset_password():
-    if request.method == 'GET':
-        return render_template('reset-password.html')
+# @app.route('/reset-passowrd', methods=[ "GET","POST" ])
+# def reset_password():
+#     if request.method == 'GET':
+#         return render_template('reset-password.html')
 
-    old_password=request.form['oldPassword']
-    new_password=request.form['newPassword']
+#     old_password=request.form['oldPassword']
+#     new_password=request.form['newPassword']
 
-    if not current_user.check_my_password(old_password):
-        Print('Old password not correct', 'danger')
+#     if not current_user.check_my_password(old_password):
+#         Print('Old password not correct', 'danger')
 
-    else:
-        current_user.hash_my_password(new_password)
-        db.session.add(current_user)
-        db.session.commit()
-        print('Password changed successfully', 'success')
+#     else:
+#         current_user.hash_my_password(new_password)
+#         db.session.add(current_user)
+#         db.session.commit()
+#         print('Password changed successfully', 'success')
 
-    return render_template('reset-password.html')
+#     return render_template('reset-password.html')
--- app/blueprints/blog/models.py
+++ app/blueprints/blog/models.py
@@ -1,6 +1,7 @@
 from app import db, login_manager
 from datetime import datetime
 from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model, UserMixin):
     id=db.Column(db.Integer, primary_key=True)
@@ -9,6 +10,12 @@ class User(db.Model, UserMixin):
     user_name=db.Column(db.String(50), unique=True)
     first_name=db.Column(db.String(50)) 
     last_name=db.Column(db.String(50))
+
+    def hash_my_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_my_password(self, password):
+        return check_password_hash(self.password, password)
 
 
 class Car(db.Model):
