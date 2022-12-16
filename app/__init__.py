849e2811c73ef0a98ff2fc1173f9664d777e7d20
Shawn <sgiovanni1@yahoo.com>
2022-12-15 22:14:52 -0500

    Added gunicorn

--- app/__init__.py
+++ app/__init__.py
@@ -7,29 +7,34 @@ from flask_login import LoginManager
 db =SQLAlchemy()
 migrate=Migrate()
 login_manager = LoginManager()
-app=Flask (__name__)
 
-app.config.from_object(Config)
 
-db.init_app(app)
-migrate.init_app(app,db)
-login_manager.init_app(app)
+def create_app():
+    app=Flask (__name__)
 
-# from app import routes,models
-from app.blueprints.main import bp as main_bp
-app.register_blueprint(main_bp)
+    app.config.from_object(Config)
 
-from app.blueprints.blog import bp as blog_bp
-app.register_blueprint(blog_bp)
+    db.init_app(app)
+    migrate.init_app(app,db)
+    login_manager.init_app(app)
 
-from app.blueprints.auth import bp as auth_bp
-app.register_blueprint(auth_bp)
+    # from app import routes,models
+    from app.blueprints.main import bp as main_bp
+    app.register_blueprint(main_bp)
 
-# from app.blueprints.api import bp as api_bp
-# app.register_blueprint(api_bp)
+    from app.blueprints.blog import bp as blog_bp
+    app.register_blueprint(blog_bp)
 
-login_manager.login_view = 'auth.login'
-login_manager.login_message = 'need permissions for this page'
-login_manager.login_message_category = 'danger'
+    from app.blueprints.auth import bp as auth_bp
+    app.register_blueprint(auth_bp)
+
+    # from app.blueprints.api import bp as api_bp
+    # app.register_blueprint(api_bp)
+
+    login_manager.login_view = 'auth.login'
+    login_manager.login_message = 'need permissions for this page'
+    login_manager.login_message_category = 'danger'
+
+    return app
 
 
